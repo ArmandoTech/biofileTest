19e9d6be6040e27cd6e85ea8431512e45478e5a2
Armando <104610494+ArmandoTech@users.noreply.github.com>
2022-10-12 17:52:39 -0500

    refactor/cleaner code

--- sayHello.js
+++ sayHello.js
@@ -1,5 +1,9 @@
 const sayHello = (name) => {
-  const date = new Date();
-  const time = `${date.getHours()}:${date.getMinutes()}`;
-  alert(`Buenas tardes ${name}!, son las ${time}`);
+  const time = new Date();
+  const hour = time.toLocaleString("en-US", {
+    hour: "numeric",
+    minute: "numeric",
+    hour12: true,
+  });
+  console.log(`Buenas tardes ${name}!, son las ${hour}`);
 };
